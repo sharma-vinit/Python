93b79c2154fefb050b65b62dfbcb839ec8ec96ce
vinit <vinit123@gmail.com>
2020-03-25 02:02:17 +0530

    OOP

--- com/Test.py
+++ com/Test.py
@@ -1,3 +1,15 @@
-l = [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
-for x, xsquared in l:
-    print(x, ':', xsquared)
+def memory_locations_of(value):
+    for index, element in enumerate(value):
+        print("{: <15}{}".format("index[" + str(index) + "] ", hex(id(element))))
+        print("{: <15}{}".format(str(element), hex(id(str(element)))))
+
+
+python_list = [0, 1, 2]
+memory_locations_of(python_list)
+
+# import numpy library
+import numpy as np
+
+# Create a numpy array
+numpy_list = np.array([0, 1, 2], dtype='int32')
+memory_locations_of(numpy_list)
